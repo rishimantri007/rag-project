62d8439f86ec9e77958f8b69b3fc31fcc0db8518
rishimantri007 <rishielec123@gmail.com>
2026-06-07 16:37:38 +0530

    Recursiv

--- document loaders/pdf.py
+++ document loaders/pdf.py
@@ -2,12 +2,12 @@ from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_text_splitters import TokenTextSplitter
+from langchain_text_splitters import TokenTextSplitter, RecursiveCharacterTextSplitter
 
 data = PyPDFLoader("document loaders/GRU.pdf")
 
 docs = data.load()
-splitter = TokenTextSplitter(chunk_size=100, chunk_overlap=0)
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 chunks = splitter.split_documents(docs)
 
-print(len(chunks))
+print((chunks[0].page_content))
